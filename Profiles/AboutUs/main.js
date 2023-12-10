f6da0acbf91352df5c98220f958800af003e5d34
veth14 <vianangelo.14@gmail.com>
2023-12-11 03:11:19 +0800

    Landing Page Added

---
+++ Profiles/AboutUs/main.js
@@ -0,0 +1,66 @@
+// change navbar on scroll
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+
+// Show/hide faq questions 
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'fa-solid fa-plus'){
+            icon.className =  'fa-solid fa-minus';   
+        }else{
+            icon.className = 'fa-solid fa-plus';
+        }
+    })
+});
+
+
+//show/hide navbar
+
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+// close navbar
+
+const closeNav = () =>{
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
